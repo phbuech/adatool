5f594f838b9f15b30c391d44477ee429b7145b7a
Philipp Buech <72556579+phbuech@users.noreply.github.com>
2024-03-25 02:21:22 +0100

    Windows installation script added
    Additional script for the setup of the python environment

---
+++ installation_scripts/Windows_install_ADA.bat
@@ -0,0 +1,22 @@
+ECHO This script installs a Python distribution [3.11], an environment and the dependencies for ADA using conda. Please follow the instructions.
+ECHO Do you want to execute the installation script [yes/no]?
+SET /p "reply=reply: "
+IF %reply%==yes (
+    C:\Users\%USERNAME%\anaconda3\Scripts\activate base
+    IF EXIST C:\Users\%USERNAME%\anaconda3\envs\ada_env\ (
+        ECHO Remove ada environment
+        ECHO y | conda remove --name ada_env --all        
+    )
+    ECHO install ada environment
+    ECHO y | conda create --name ada_env Python=3.11
+    ECHO Activate ada environment
+    conda activate ada_env
+    ECHO Install dependencies
+    ECHO y | pip3 install -r %~dp0\requirements.txt
+
+    ) else ( ECHO Ada environment not installed. )
+
+
+ECHO Ada environment installed
+
+pause
